319585256fd77c8f2192734d3b9732b1164185b2
Aria Samandi <ariasamandi@gmail.com>
2018-06-23 15:09:38 -0700

    commit

---
+++ src/main/java/com/codingdojo/authentication/users/Users.java
@@ -0,0 +1,81 @@
+package com.codingdojo.authentication.users;
+
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.codingdojo.authentication.models.User;
+import com.codingdojo.authentication.services.UserService;
+import com.codingdojo.authentication.validator.UserValidator;
+
+// imports removed for brevity
+@Controller
+public class Users {
+    private final UserService userService;
+    
+    // NEW
+    private final UserValidator userValidator;
+    
+    // NEW
+    public Users(UserService userService, UserValidator userValidator) {
+        this.userService = userService;
+        this.userValidator = userValidator;
+    }
+        
+    
+    @RequestMapping("/registration")
+    public String registerForm(@ModelAttribute("user") User user) {
+        return "registrationPage.jsp";
+    }
+    @RequestMapping("/login")
+    public String login() {
+        return "loginPage.jsp";
+    }
+    @RequestMapping(value="/registration", method=RequestMethod.POST)
+    public String registerUser(@Valid @ModelAttribute("user") User user, BindingResult result, HttpSession session) {
+        userValidator.validate(user, result);
+        if(result.hasErrors()) {
+            return "registrationPage.jsp";
+        }
+        User u = userService.registerUser(user);
+        session.setAttribute("user_id", u.getId());
+        return "redirect:/home";
+    }
+    
+    @RequestMapping(value="/login", method=RequestMethod.POST)
+    public String loginUser(@RequestParam("email") String email, @RequestParam("password") String password, Model model, HttpSession session) {
+        // if the user is authenticated, save their user id in session
+        // else, add error messages and return the login page
+    	if(userService.authenticateUser(email, password)) {
+    		User user = userService.findByEmail(email);
+    		session.setAttribute("user_id", user.getId());
+    		return "redirect:/home";
+    	}
+    	else {
+    		return "redirect:/login";
+    	}
+    }
+    
+    @RequestMapping("/home")
+    public String home(HttpSession session, Model model) {
+        // get user from session, save them in the model and return the home page
+    	Long user_id = (Long) session.getAttribute("user_id");
+    	User user = userService.findUserById(user_id);
+    	model.addAttribute("user", user);
+    	return "homePage.jsp";
+    }
+    @RequestMapping("/logout")
+    public String logout(HttpSession session) {
+        // invalidate session
+        // redirect to login page
+    	session.invalidate();
+    	return "redirect:/login";
+    }
+}
